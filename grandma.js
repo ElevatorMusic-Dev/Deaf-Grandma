49db6acd8dcad2174f943eaa59dacfe277ff0c1a
ElevatorMusic-Dev <ElevatorMusicDev@proton.me>
2023-02-02 22:42:52 -0600

    added styling, js functionality.

---
+++ grandma.js
@@ -0,0 +1,13 @@
+
+  let randomYear = Math.floor(Math.random() * (1969 - 1922) + 1922); //generates a random year
+
+
+
+const grandmaChat = function() {
+    let userText =  document.getElementById("userText").value; //get users entered text
+    if (userText === userText.toUpperCase()) {
+        document.getElementById("grandmaText").value = `NOT SINCE ${randomYear}!`;
+    } else {
+        document.getElementById("grandmaText").value = "WHATS THAT DEARY!?";
+    }
+}
