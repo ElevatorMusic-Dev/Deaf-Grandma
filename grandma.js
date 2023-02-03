2806abee2fe4f6422e6f5948b31ee58376f492c3
ElevatorMusic-Dev <ElevatorMusicDev@proton.me>
2023-02-03 00:28:04 -0600

    added randomYear to function, fixed  year repeat

--- grandma.js
+++ grandma.js
@@ -1,9 +1,11 @@
+function getRandomYear() {
 let randomYear = Math.floor(Math.random() * (1969 - 1922) + 1922); //generates a random year
-
+return randomYear;
+}
 const grandmaChat = function () {
   let userText = document.getElementById("userText").value; //get users entered text
   if (userText === userText.toUpperCase()) {
-    document.getElementById("grandmaText").value = `NOT SINCE ${randomYear}!`;
+    document.getElementById("grandmaText").value = `NOT SINCE ${getRandomYear()}!`;
   } else {
     document.getElementById("grandmaText").value = "WHATS THAT DEARY!?";
   }
