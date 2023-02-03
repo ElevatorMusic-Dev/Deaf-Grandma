0b9e275a37f3d4db847a574bf92b00e846ea8802
ElevatorMusic-Dev <ElevatorMusicDev@proton.me>
2023-02-02 22:45:09 -0600

    cleaned up formatting

--- grandma.js
+++ grandma.js
@@ -1,13 +1,10 @@
+let randomYear = Math.floor(Math.random() * (1969 - 1922) + 1922); //generates a random year
 
-  let randomYear = Math.floor(Math.random() * (1969 - 1922) + 1922); //generates a random year
-
-
-
-const grandmaChat = function() {
-    let userText =  document.getElementById("userText").value; //get users entered text
-    if (userText === userText.toUpperCase()) {
-        document.getElementById("grandmaText").value = `NOT SINCE ${randomYear}!`;
-    } else {
-        document.getElementById("grandmaText").value = "WHATS THAT DEARY!?";
-    }
-}
+const grandmaChat = function () {
+  let userText = document.getElementById("userText").value; //get users entered text
+  if (userText === userText.toUpperCase()) {
+    document.getElementById("grandmaText").value = `NOT SINCE ${randomYear}!`;
+  } else {
+    document.getElementById("grandmaText").value = "WHATS THAT DEARY!?";
+  }
+};
